a28df04b926ef0e4207eeae249b70ebd5c3e43a3
Mai Anh Vu <me@maianhvu.com>
2017-10-07 00:27:51 +0800

    remove redundant filters

--- src/processing/XmlParseFilter.js
+++
@@ -1,8 +0,0 @@
-const Filter = require('../architecture/Filter')
-const parser = require('xml2json')
-
-module.exports = class XmlParseFilter extends Filter {
-    processData(data) {
-        return parser.toJson(data, { object: true, coerce: true })
-    }
-}
